bb56fe1f70edcc397c963ad0a6f211d870f39a73
mvgagenciasac-eng <mvg.agencia.sac@gmail.com>
2026-05-06 18:25:13 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 require("dotenv").config();
 const express = require("express");
 const axios = require("axios");
+const cors = require("cors");
 
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 
 // Teste no navegador

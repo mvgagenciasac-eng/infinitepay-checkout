0587e788f50d53110d8a6a5aca43ab6b56209f06
mvgagenciasac-eng <mvg.agencia.sac@gmail.com>
2026-05-07 12:27:14 -0300

    Update server.js

--- server.js
+++ server.js
@@ -696,14 +696,14 @@ app.post("/checkout", async (req, res) => {
             </label>
 
             <button class="pay-button">
-              <span class="lock">🔒</span>
+              <span class="lock"></span>
               <span>Ir para o pagamento</span>
               <span class="button-divider"></span>
-              <img loading="eager" src="https://cdn.prod.website-files.com/65c1399ac999a342139b5069/65c1399ac999a342139b5434_logo_brlc_preto.svg" alt="Logo InfinitePay" class="img-logo_infinitepay w-variant-c74a8267-4291-3686-2047-6a4b3b9bb8fa is-responsive">
+              
             </button>
 
             <div class="secure-note">
-              🔒 Seus dados estão protegidos e o pagamento é 100% seguro.
+            Seus dados estão protegidos <img loading="eager" src="https://cdn.prod.website-files.com/65c1399ac999a342139b5069/65c1399ac999a342139b5434_logo_brlc_preto.svg" alt="Logo InfinitePay" class="img-logo_infinitepay w-variant-c74a8267-4291-3686-2047-6a4b3b9bb8fa is-responsive">
             </div>
           </section>
 
